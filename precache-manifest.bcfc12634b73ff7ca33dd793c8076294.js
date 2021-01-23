f687841c7a0be5a7efeca56aa87ada268d9025b6
George Vasilescu <geo.vpg1@gmail.com>
2021-01-23 16:43:00 +0000

    Add files via upload

---
+++ precache-manifest.bcfc12634b73ff7ca33dd793c8076294.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0effecc5ae79aba86786411712b4708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2405fd80c3afe4bf29f1",
+    "url": "/static/css/main.2cf75415.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2405fd80c3afe4bf29f1",
+    "url": "/static/js/main.28bab72c.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
